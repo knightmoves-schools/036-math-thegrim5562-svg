8103c135fb9c7a951ecbf68e154765ce5d070503
raishaun cooke <thegrim5562@gmail.com>
2026-05-18 10:43:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let total = 1 + 4;
-// total = total - 2;
-// total = total * 5;
+total = total - 2;
+total = total * 5;
 
 document.getElementById("result").innerHTML = total / 2;
 document.getElementByI("Remainder").innerHTML = total % 2;
-
