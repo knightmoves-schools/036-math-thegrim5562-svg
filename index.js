024279917af0a94f5c08d3471e68016200d1d061
raishaun cooke <thegrim5562@gmail.com>
2026-05-18 10:39:56 -0600

    Remove commented code and simplify calculations

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ let total = 1 + 4;
 document.getElementById("result").innerHTML = total / 2;
 document.getElementByI("Remainder").innerHTML = total % 2;
 
-should capture the result of 1 plus 4 in a variable named sum
-should capture the result of sum minus 2 in a variable named difference
-should capture the result of difference multiplied by 5 in a variable named product
-should capture the result of product divided by 2 in a variable named quotient
-should capture the remainder of product remainder of 2 in a variable named remainder
-should assign the innerHTML of the HTML element with the id result to the quotient
